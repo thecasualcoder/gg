51f5333632ddfb1ac4de3737d8d1cb697d4cfd4f
Ninan John <jninan@thoughtworks.com>
2019-06-22 21:34:00 +0530

    [Ninan][WIP] Introduce git2 crate to find out status of each git directory

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use walkdir::WalkDir;
-use std::path::PathBuf;
+use git2::{Repository, StatusOptions};
+use std::path::{PathBuf, Path};
 
 fn main() {
-    let mut git_dirs: Vec<_> = vec![];
+    let mut all_git_dirs = vec![];
+
     for entry in WalkDir::new("/Users/ninanjohn/Trials")
         .follow_links(true)
         .contents_first(true)
@@ -10,9 +12,35 @@ fn main() {
         {
             let entry = entry.unwrap();
             if entry.file_name().eq(".git") {
-                git_dirs.push(entry);
+                all_git_dirs.push(entry.clone());
+                let path = Path::new(entry.path().parent().unwrap());
+                let mut opts = StatusOptions::new();
+                let repo = Repository::open(path);
+                let repo = repo.unwrap();
+                if repo.is_bare() {
+                    println!("{:#?}: bare", entry.path().parent().unwrap());
+                    continue
+                }
+
+                opts.include_ignored(true)
+                    .include_untracked(true)
+                    .recurse_untracked_dirs(false)
+                    .exclude_submodules(false);
+
+                match repo.statuses(Some(&mut opts)) {
+                    Ok(s) => {
+                        // Todo: Filter the statuses to find untracked, new, deleteions, renames...
+                        // Todo: https://github.com/rust-lang/git2-rs/blob/master/examples/status.rs
+                        // Todo: Line 150
+                        // Currently this does not filter anything.
+                        println!("{:#?}: untracked", entry.path().parent().unwrap());
+                    }
+                    Err(e) => {
+                        panic!("Error in getting status for entry: {:#?}", entry.clone().path().parent().unwrap())
+                    }
+                }
                 continue;
             }
+
         }
-    println!("{:#?}", git_dirs);
 }
