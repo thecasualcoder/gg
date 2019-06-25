b719e51de9e784df7f08038d1bd6d2352059f2f2
Ninan John <jninan@thoughtworks.com>
2019-06-25 08:57:37 +0530

    [Ninan] Show all types of status for each git directory in given path

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use walkdir::WalkDir;
-use git2::{Repository, StatusOptions};
-use std::path::{PathBuf, Path};
+use std::path::{Path, PathBuf};
+
 use colored::*;
+use git2::{Repository, StatusOptions};
+use walkdir::WalkDir;
 
 fn main() {
     let mut all_git_dirs = vec![];
@@ -29,12 +30,36 @@ fn main() {
                     .exclude_submodules(false);
 
                 match repo.statuses(Some(&mut opts)) {
-                    Ok(s) => {
-                        // Todo: Filter the statuses to find untracked, new, deleteions, renames...
-                        // Todo: https://github.com/rust-lang/git2-rs/blob/master/examples/status.rs
-                        // Todo: Line 150
-                        // Currently this does not filter anything.
-                        println!("{:#?}: {}", entry.path().parent().unwrap(), "untracked".red());
+                    Ok(statuses) => {
+                        let mut statuses_in_dir = vec![];
+                        for entry in statuses
+                            .iter()
+                            .filter(|e| e.status() != git2::Status::CURRENT)
+                            {
+                                let status = &entry.status();
+                                if git2::Status::is_wt_new(status) {
+                                    statuses_in_dir.push("new files")
+                                }
+                                if git2::Status::is_wt_deleted(status) {
+                                    statuses_in_dir.push("deletions");
+                                }
+                                if git2::Status::is_wt_renamed(status) {
+                                    statuses_in_dir.push("renames");
+                                }
+                                if git2::Status::is_wt_typechange(status) {
+                                    statuses_in_dir.push("typechanges");
+                                }
+                                if git2::Status::is_wt_modified(status) {
+                                    statuses_in_dir.push("modifications");
+                                }
+                            }
+                        if statuses_in_dir.is_empty() {
+                            println!("{:#?}: {}", entry.path().parent().unwrap(), "no changes".green());
+                        } else {
+                            statuses_in_dir.sort();
+                            statuses_in_dir.dedup();
+                            println!("{:#?}: {}", entry.path().parent().unwrap(), statuses_in_dir.join(", ").red());
+                        }
                     }
                     Err(e) => {
                         panic!("Error in getting status for entry: {:#?}", entry.clone().path().parent().unwrap())
