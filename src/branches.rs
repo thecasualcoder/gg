744e1171882d86aa2aca2831e47a20103003ad5e
Ninan John <jpninanjohn@delium.co>
2020-08-17 20:59:12 +0530

    [Ninan] Compare each branch to the main branch and print their status

--- src/branches.rs
+++ src/branches.rs
@@ -1,10 +1,10 @@
-use std::path::PathBuf;
-
 use clap::{App, Arg, SubCommand};
+use colored::*;
 use git2::{Error as GitError, Repository};
 use git2::BranchType::Local;
 
 use crate::input_args::InputArgs;
+use std::process;
 
 pub fn sub_command<'a, 'b>() -> App<'a, 'b> {
     SubCommand::with_name("branches")
@@ -19,30 +19,72 @@ pub fn sub_command<'a, 'b>() -> App<'a, 'b> {
 
 pub fn branches(args: InputArgs) {
     let root_path = args.get_root_path("repo_path");
+    let repo = Repository::open(root_path).expect("Failed to open git repo");
 
     // Todo: Use this to do the comparisons
-    let mut branches = GitBranches {
-        local_path: root_path,
-        main_branch: String::from("master"),
+    let mut git_branches = GitBranches {
+        repo: repo,
+        main_branch: String::from("origin/master"),
     };
 
-    let _branches = branches.get_branches();
+    let branches = git_branches.get_branches().unwrap_or_else(|err| {
+        println!("{} {}", "Failed to get local branches. Err:".red(), err);
+        process::exit(1);
+    });
+
+    // print!("{:#?}", branches);
+    branches
+        .into_iter()
+        .for_each(|branch| {
+            if branch == "master" {
+                return;
+            }
+            git_branches.compare_to_main_branch(branch.as_str())
+        })
 }
 
 pub struct GitBranches {
-    local_path: PathBuf,
+    repo: Repository,
     main_branch: String,
 }
 
 impl GitBranches {
     fn get_branches(&mut self) -> Result<Vec<String>, GitError> {
-        let repo = Repository::open(self.local_path.clone())?;
-        let local_branches = repo.branches(Some(Local))?;
+        let local_branches = self.repo.branches(Some(Local))?;
+        let mut branch_vec = vec![];
         local_branches.for_each(|br| {
             let branch_ref = br.expect("Failed to get branch");
-            let branch_name = branch_ref.0.name().expect("Failed to get valid name of branch").unwrap_or("");
-            println!("{}", branch_name)
+            let branch_name = branch_ref.0.name().expect("Failed to get valid name of branch")
+                .expect("Failed to convert branch name to string");
+            branch_vec.push(branch_name.to_string())
         });
-        Ok(vec![])
+        Ok(branch_vec)
+    }
+
+    fn compare_to_main_branch(&mut self, branch: &str) {
+        let mut branch_status = vec![];
+        self.repo.revparse_single(branch).map_or_else(|_err| {
+            print!("{} {} {}", "No such branch".red(), branch.red(), "found".red());
+        }, |reference|{
+            let head_ref = reference.id();
+            let (is_ahead, is_behind) = self.repo
+                .revparse_ext(self.main_branch.as_str())
+                .ok()
+                .and_then(|(upstream, _)| self.repo.graph_ahead_behind(head_ref, upstream.id()).ok())
+                .unwrap_or((0, 0));
+
+            if is_ahead > 0 {
+                let ahead = format!("{} ahead", is_ahead);
+                let ahead_string_colored = format!("{}", ahead.green());
+                branch_status.push(ahead_string_colored);
+            }
+
+            if is_behind > 0 {
+                let behind = format!("{} behind", is_behind);
+                let behind_string_colored = format!("{}", behind.yellow());
+                branch_status.push(behind_string_colored);
+            }
+            println!("{} {}: {}", "Branch".blue(), branch.blue(), branch_status.join(", "));
+        });
     }
 }
